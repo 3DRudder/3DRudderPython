f10f7b0d2e40b5e451e5c8d0db31efd597f61b1b
Unknown <mvivant@3drudder.com>
2017-07-18 09:25:13 +0200

    Add 32 and 64 bit SDK

--- sample_02.py
+++ sample_02.py
@@ -9,10 +9,17 @@
 # Copyright (C) 2010-2017 3dRudder
 #
 #####################################################################################
-from ns3DRudder import * #import SDk 3dRudder
 import time
+import platform
 
+# 32 or 64 bit
+val_max=platform.architecture()
 
+print(val_max[0])
+if (val_max[0]=='32bit') : 
+    from win32.ns3DRudder import * #import SDk 3dRudder
+else:
+    from x64.ns3DRudder import * #import SDk 3dRudder
 
 #define the Status of the 3dRudder
 status_3dRudder = [ "None",
